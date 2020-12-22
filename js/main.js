39e9f7d413895ce7aec233cf19f016f929dfb31d
Aman Singh <imaman07singh@gmail.com>
2020-12-22 17:11:54 +0530

    timeline-1

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+// for small dots to show active
+$(".step").click(function () {
+    $(this).addClass("tl-active").prevAll().addClass("tl-active");
+    $(this).nextAll().removeClass("tl-active");
+});
+
+
+var totalSteps = $(".step");
+var division = 100 / (totalSteps.length - 1);
+
+function activeFunc(step) {
+
+    //for progress-line animation
+    if (step == 1) {
+        $("#progress-line").css("width", "3%");
+    }
+    else {
+        $("#progress-line").css("width", ((step - 1) * division) + "%");
+    }
+    // showing the content
+    $(".content-timeline").removeClass("cnt-active");
+    $(".content" + step).addClass("cnt-active");
+
+}
