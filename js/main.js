a54828656e6fd19c1d84779765b4baa8a083e5c8
Aman Singh <imaman07singh@gmail.com>
2020-12-24 13:31:57 +0530

    nav

--- js/main.js
+++ js/main.js
@@ -7,6 +7,39 @@ function showpage() {
     $("#mother-container").css("display", "block");
 }
 
+
+// NAVBAR
+var sidebar_i = $(".sidebar li i");
+var sidebar_span = $(".sidebar li span");
+var nav = $("#nav");
+var nav_ham = $("#hamburger");
+var nav_close = $("#nav #nav-close");
+
+$("#hamburger").click(function () {
+
+    $(".sidebar").animate({ width: '180px' }, 300, function () {
+        sidebar_span.css("display", "inline");
+        nav_ham.hide();
+        nav_close.show();
+    });
+    nav.animate({ width: '180px' }, 300);
+    sidebar_i.css("width", "30%");
+
+});
+
+$("#nav-close").click(function () {
+
+    sidebar_span.css("display", "none");
+    nav.animate({ width: '70px' }, 300);
+    sidebar_i.css("width", "100%");
+
+    $(".sidebar").animate({ width: '70px' }, 300, function () {
+        nav_ham.show();
+        nav_close.hide();
+    });
+});
+
+
 // for small dots to show active
 $(".step").click(function () {
     $(this).addClass("tl-active").prevAll().addClass("tl-active");
