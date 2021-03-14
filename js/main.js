2e7582be72a361044e437bc5febba928042492ae
UtkarshKeerti <utkarsh.keerti@gmail.com>
2021-03-14 19:36:02 +0530

    Nav: Ripple effect added

--- js/main.js
+++ js/main.js
@@ -255,3 +255,34 @@ var swiper = new Swiper('.swiper-container', {
   }
 
 });
+
+// RIPPLE EFFECT
+
+$(".nav-li").on("click", function (e) {
+
+  $(".atg-ripple").remove();
+
+  var posX = $(this).offset().left,
+      posY = $(this).offset().top,
+      buttonWidth = $(this).width(),
+      buttonHeight =  $(this).height();
+  
+  $(this).prepend("<span class='atg-ripple'></span>");
+
+  if(buttonWidth >= buttonHeight) {
+    buttonHeight = buttonWidth;
+  } else {
+    buttonWidth = buttonHeight; 
+  }
+  
+  var x = e.pageX - posX - buttonWidth / 2;
+  var y = e.pageY - posY - buttonHeight / 2;
+
+  $(".atg-ripple").css({
+    width: buttonWidth,
+    height: buttonHeight,
+    top: y + 'px',
+    left: x + 'px'
+  }).addClass("atg-rippleEffect");
+});
+
